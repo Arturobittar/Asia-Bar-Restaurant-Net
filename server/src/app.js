595d01304d354875ef0f79cfb9e0e8056da07b86
Arturobittar <pauloarturomata@gmail.com>
2025-10-28 09:48:11 -0400

    se me olvidó este archivo jejejeje

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ import morgan from 'morgan';
 import CookieParser from 'cookie-parser';
 import authRouter from './routes/auth.routes.js';
 import crudRouter from './routes/crud.routes.js';
+import tipoCambioRouter from './routes/tipoCambio.routes.js';
 import cors from 'cors';
 
 const app = express();
@@ -26,5 +27,6 @@ app.use(CookieParser());
 
 app.use('/api', authRouter);
 app.use('/api', crudRouter);
+app.use('/api/tipo-cambio', tipoCambioRouter);
 
 export default app;
